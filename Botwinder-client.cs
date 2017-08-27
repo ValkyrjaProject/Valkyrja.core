6156703e61b9a3b4a7cfa318946f28417afa6b52
Radka Janek <radka.janek@redhat.com>
2017-08-27 04:44:08 +0200

    Added discord.net reference.

--- Botwinder-client.cs
+++ Botwinder-client.cs
@@ -5,5 +5,18 @@ namespace Botwinder.core
 {
 	public partial class BotwinderClient<TUser> : IDisposable where TUser : UserData, new()
 	{
+
+		public BotwinderClient()
+		{
+			//load main json config
+
+		}
+
+
+
+		public void Dispose()
+		{
+			//todo
+		}
 	}
 }
