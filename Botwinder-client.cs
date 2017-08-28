b01e75ca5f6f708c92892dc0fc41fefe75b1d572
Radka Janek <radka.janek@redhat.com>
2017-08-29 01:56:52 +0200

    Basic Db stuff and test

--- Botwinder-client.cs
+++ Botwinder-client.cs
@@ -1,24 +1,45 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Botwinder.entities;
-
+using Microsoft.EntityFrameworkCore;
 using guid = System.Int64;
 
 namespace Botwinder.core
 {
 	public partial class BotwinderClient<TUser> : IDisposable where TUser : UserData, new()
 	{
+		private readonly DbConfig DbConfig;
+		private readonly GlobalContext GlobalDb;
+		private GlobalConfig GlobalConfig;
 
 		public BotwinderClient()
 		{
-			//load main json config
-
+			this.DbConfig = DbConfig.Load();
+			this.GlobalDb = GlobalContext.Create(this.DbConfig.GetDbConnectionString());
 		}
 
-
+#pragma warning disable 1998
+		public async Task<string> TestDb()
+		{
+			return "Loaded global config: " + this.GlobalConfig.ConfigName;
+		}
+#pragma warning restore 1998
 
 		public void Dispose()
 		{
 			//todo
+		}
+
+		public void Connect()
+		{
+			ReloadConfig();
+
+		}
+
+		public void ReloadConfig()
+		{
+			this.GlobalConfig = this.GlobalDb.GlobalConfigs.First();
 		}
 	}
 }
