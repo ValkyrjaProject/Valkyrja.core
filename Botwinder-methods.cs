8721c617e0d81f3c60d597b5188ede7bec54a7bf
Radka Janek <radka.janek@redhat.com>
2017-08-28 07:25:57 +0200

    +EFCore

--- Botwinder-methods.cs
+++ Botwinder-methods.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Botwinder.entities;
 
+using guid = System.Int64;
+
 namespace Botwinder.core
 {
 	public partial class BotwinderClient<TUser> : IDisposable where TUser : UserData, new()
