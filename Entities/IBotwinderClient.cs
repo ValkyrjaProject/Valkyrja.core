0377324d6414098e4eea4151f2bcace7a43198a1
Radka Janek <radka.janek@redhat.com>
2017-09-04 14:32:12 +0200

    Moved the Entities project to the Core repo

---
+++ Entities/IBotwinderClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using guid = System.Int64;
+
+namespace Botwinder.entities
+{
+	public interface IBotwinderClient<TUser> where TUser : UserData, new()
+	{
+		Task LogException(Exception exception, CommandArguments<TUser> args);
+		Task LogException(Exception exception, string data, guid serverId = 0);
+	}
+}
