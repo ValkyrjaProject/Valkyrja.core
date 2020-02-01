eb16e33a825d4220037662dda3bc85d4697fe43f
RheaAyase <radka.janek@redhat.com>
2020-02-01 12:36:46 +0100

    count bans&stuff

--- Entities/Monitoring.cs
+++ Entities/Monitoring.cs
@@ -11,7 +11,8 @@ namespace Valkyrja.entities
 		public readonly Counter Error500s = Metrics.CreateCounter("discord_valk_500", "Valkyrja: Discord server error 500s");
 		public readonly Counter Messages = Metrics.CreateCounter("discord_valk_msg", "Valkyrja: Messages received");
 		public readonly Counter Commands = Metrics.CreateCounter("discord_valk_cmd", "Valkyrja: Commands executed");
-		public readonly Counter AntispamDeletes = Metrics.CreateCounter("discord_valk_del", "Valkyrja: Messages deleted by Antispam");
+		public readonly Counter AntispamActions = Metrics.CreateCounter("discord_valk_antispam", "Valkyrja: Antispam actions");
+		public readonly Counter MsgsDeleted = Metrics.CreateCounter("discord_valk_del", "Valkyrja: Messages deleted");
 		public readonly Counter Mutes = Metrics.CreateCounter("discord_valk_mute", "Valkyrja: Mutes issued");
 		public readonly Counter Bans = Metrics.CreateCounter("discord_valk_ban", "Valkyrja: Bans issued");
 
