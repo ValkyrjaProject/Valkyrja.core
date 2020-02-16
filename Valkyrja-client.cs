786c280efbae84578342640ac6f1a15543d36052
RheaAyase <radka.janek@redhat.com>
2020-02-16 20:44:28 +0100

    give me a second?

--- Valkyrja-client.cs
+++ Valkyrja-client.cs
@@ -501,7 +501,7 @@ namespace Valkyrja.core
 					if( this.GlobalConfig.LogDebug )
 						Console.WriteLine("ValkyrjaClient: Update loop triggered at: " + Utils.GetTimestamp(DateTime.UtcNow));
 
-					this.Monitoring.Uptime.Set((DateTime.UtcNow - this.TimeStarted).Seconds);
+					this.Monitoring.Uptime.Set((DateTime.UtcNow - this.TimeStarted).TotalSeconds);
 					await Update();
 				}
 				catch(Exception exception)
